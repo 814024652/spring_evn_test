61f22d22cca799a53fb1a2156a403ff9c69d8fab
zhongrun <814024652@qq.com>
2020-04-27 14:07:10 +0800

    test

--- src/main/java/com/zhong/test/evndemo/controller/TestController.java
+++ src/main/java/com/zhong/test/evndemo/controller/TestController.java
@@ -1,12 +1,10 @@
 package com.zhong.test.evndemo.controller;
 
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,7 +15,6 @@ public class TestController {
 
     @GetMapping("/evn")
     public Map getDen() {
-
         Map res = new HashMap();
         res.put("evn", "");
         return res;
