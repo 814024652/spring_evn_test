e87b7147f8c393ea24edaeafe3e280211a0b6c39
zhongrun <814024652@qq.com>
2020-05-07 15:03:45 +0800

    test

--- src/main/java/com/zhong/test/evndemo/controller/TestController.java
+++ src/main/java/com/zhong/test/evndemo/controller/TestController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,9 +16,9 @@ public class TestController {
 
 
     @GetMapping("/evn")
-    public Map getDen() {
+    public Map getDen() throws UnknownHostException {
         Map res = new HashMap();
-        res.put("evn", "");
+        res.put("evn", Inet4Address.getLocalHost().getHostAddress());
         return res;
     }
 
